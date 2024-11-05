4154f488c04e3588c93e74ce1c8bc402d3f25fb6
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2024-11-05 15:23:54 +0100

    proxy for backend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,9 +5,23 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  optimizeDeps: {
+    exclude: ["js-big-decimal"],
   },
 });
